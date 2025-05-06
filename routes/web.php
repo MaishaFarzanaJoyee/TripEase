8fb05a47c98965aba4cf5b741ab32e4c375d9268
Eshat-Tanzeem <eshat.tanzeem@g.bracu.ac.bd>
2025-05-06 18:08:34 +0600

    shows cart, available tickets, process checout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+Route::post('/tickets/{ticket}/add-to-cart', [TicketController::class, 'addToCart'])->name('tickets.addToCart');
+Route::get('/cart', [TicketController::class, 'viewCart'])->name('tickets.cart');
+Route::post('/checkout', [TicketController::class, 'checkout'])->name('tickets.checkout');
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
