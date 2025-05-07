b37e974b37860e0cfdd591b0b3584947277d8738
Eshat-Tanzeem <eshat.tanzeem@g.bracu.ac.bd>
2025-05-07 22:26:02 +0600

    web.php budget

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\BudgetController;
 
 Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
 Route::post('/tickets/{ticket}/add-to-cart', [TicketController::class, 'addToCart'])->name('tickets.addToCart');
 Route::get('/cart', [TicketController::class, 'viewCart'])->name('tickets.cart');
 Route::post('/checkout', [TicketController::class, 'checkout'])->name('tickets.checkout');
+Route::get('/budget', [BudgetController::class, 'show'])->name('budget.show');
+Route::get('/budget/create', [BudgetController::class, 'create'])->name('budget.create');
+Route::post('/budget', [BudgetController::class, 'store'])->name('budget.store');
 
 
 
